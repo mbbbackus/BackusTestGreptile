3e457b0e8bfd805468ac98a6ab4029ac88a02627
Ben Backus <mbbbackus@gmail.com>
2025-09-09 17:53:48 -0700

    lol

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 // Utility functions
+// Whatdya want from me eh
 function formatDate(date) {
   const options = { year: 'numeric', month: 'long', day: 'numeric' };
   return date.toLocaleDateString('en-US', options);
