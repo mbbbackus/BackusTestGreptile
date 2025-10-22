771e6e33d386ea5cf7dafca2bd0b6c9269cda84e
Ben Backus <mbbbackus@gmail.com>
2025-10-22 13:49:14 -0400

    Revert "Fix more stuff"
    This reverts commit 869173af5e488c1c9c74d9f2e85e6d44e0ba8023.

--- src/loop.js
+++ src/loop.js
@@ -1,6 +1,6 @@
 for (const i = 0; i < 10; i++) {
   if (i % 2 === 0) {
-    for (const j = 0; j < 5; j++) {
+    for (let j = 0; j < 5; j++) {
       if (j > 2) {
         console.log(i + j);
       }
--- src/nest.js
+++ src/nest.js
@@ -1,8 +1,8 @@
-for (const a = 0; a < 4; a++) {
+for (let a = 0; a < 4; a++) {
   if (a > 1) {
-    for (const b = 0; b < 2; b++) {
+    for (let b = 0; b < 2; b++) {
       if (b === 0) {
-        console.log(0);
+        console.log(a * b);
       }
     }
   }
