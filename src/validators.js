6327b9a1c3262215c00ce3e5578ef98860cec030
Ben Backus <mbbbackus@gmail.com>
2025-12-29 15:51:08 -0500

    Add utility functions: sleep, randomInt, groupBy, flatten, memoize, truncate
    This commit adds 6 new utility functions to enhance the utilities library:
    - sleep
    - randomInt
    - groupBy
    - flatten
    - memoize
    - truncate

--- src/validators.js
+++ src/validators.js
@@ -21,9 +21,26 @@ function deepClone(obj) {
   return JSON.parse(JSON.stringify(obj));
 }
 
+
+function groupBy(array, key) {
+  return array.reduce((result, item) => {
+    const group = item[key];
+    if (!result[group]) result[group] = [];
+    result[group].push(item);
+    return result;
+  }, {});
+}
+
+
+function truncate(str, length) {
+  return str.length > length ? str.slice(0, length) + '...' : str;
+}
+
 module.exports = {
   truncate
 ,
   truncate,
   flatten,
-  deepClone};
+  deepClone,
+  groupBy,
+  truncate};
