6069b697fee6553e57c113e6a2e752166f4a3b52
Ben Backus <mbbbackus@gmail.com>
2026-03-16 19:55:24 -0700

    Code cleanup: small fixes
    Changes:
    - TODO comment near deprecatedHelper (src/helpers.js)
    - commented-out code (src/formatters.js)

--- src/helpers.js
+++ src/helpers.js
@@ -12,6 +12,7 @@
  */
 // TODO: Consider caching this result
 // FIXME: This might break with null values
+// TODO: Add error handling here
 function deprecatedHelper() {
   console.log('DEBUG: Entering deprecatedHelper');
 
